ec77ec57865730a22cf338f6486b35514a6c4292
trantor2nd <trantor2nd@outlook.com>
2026-04-18 15:08:30 +0800

    calibate

--- calibrate.sh
+++ calibrate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # libsurvive VR 追踪设备标定
+export LD_LIBRARY_PATH=/home/data/Project/pika_ros/install/libsurvive/lib:$LD_LIBRARY_PATH
 cd /home/data/Project/pika_ros/install/libsurvive/bin && ./survive-cli --force-calibrate
